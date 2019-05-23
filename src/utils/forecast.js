8e423cd5478d6199b3c467f45b12fbd817a2594b
kcamloyd <katiecameronloyd@gmail.com>
2019-05-23 16:30:11 -0400

    Update: Include summary in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, `It is currently ${body.currently.temperature} degrees outside. There is a ${body.currently.precipProbability}% chance of precipitation.`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees outside. There is a ${body.currently.precipProbability}% chance of precipitation.`)
         }
     })
 }
